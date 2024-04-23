28acd6509e9b62dd609fe064c60e9fcfc84a9860
Toan Phung Quoc <toanpq@moonlab.asia>
2024-04-23 16:08:45 +0700

    init project

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+import { useCustom } from "@refinedev/core";
+
+import { Col, Row } from "antd";
+
+import {
+  CompaniesMap,
+  DashboardDealsChart,
+  DashboardTasksChart,
+  DashboardTotalCountCard,
+  DashboardTotalRevenueChart,
+} from "./components";
+
+export const DashboardPage: React.FC = () => {
+  // const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+  //   url: "",
+  //   method: "get",
+  //   meta: { gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY },
+  // });
+
+  return (
+    <div className="page-container">
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="companies"
+            isLoading={false}
+            totalCount={100}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="contacts"
+            isLoading={false}
+            totalCount={100}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="deals"
+            isLoading={false}
+            totalCount={100}
+          />
+        </Col>
+      </Row>
+
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: "32px",
+        }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "432px",
+          }}
+        >
+          <DashboardTotalRevenueChart />
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={16}
+          style={{
+            height: "432px",
+          }}
+        >
+          <DashboardDealsChart />
+        </Col>
+      </Row>
+
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: "32px",
+        }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "448px",
+          }}
+        >
+          <DashboardTasksChart />
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={16}
+          style={{
+            height: "448px",
+          }}
+        >
+          <CompaniesMap />
+        </Col>
+      </Row>
+    </div>
+  );
+};
