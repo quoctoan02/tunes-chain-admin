d605f9f969424adec3eaad64f90b348d261f7da8
quoctoan02 <quoc.toan@hotmail.com>
2024-05-05 20:00:28 +0700

    remove unuse route

--- src/config/resources.tsx
+++ src/config/resources.tsx
@@ -27,26 +27,26 @@ export const resources: IResourceItem[] = [
       icon: <DashboardOutlined />,
     },
   },
-  {
-    name: "blog_posts",
-    list: "/blog-posts",
-    create: "/blog-posts/create",
-    edit: "/blog-posts/edit/:id",
-    show: "/blog-posts/show/:id",
-    meta: {
-      canDelete: true,
-    },
-  },
-  {
-    name: "categories",
-    list: "/categories",
-    create: "/categories/create",
-    edit: "/categories/edit/:id",
-    show: "/categories/show/:id",
-    meta: {
-      canDelete: true,
-    },
-  },
+  // {
+  //   name: "blog_posts",
+  //   list: "/blog-posts",
+  //   create: "/blog-posts/create",
+  //   edit: "/blog-posts/edit/:id",
+  //   show: "/blog-posts/show/:id",
+  //   meta: {
+  //     canDelete: true,
+  //   },
+  // },
+  // {
+  //   name: "categories",
+  //   list: "/categories",
+  //   create: "/categories/create",
+  //   edit: "/categories/edit/:id",
+  //   show: "/categories/show/:id",
+  //   meta: {
+  //     canDelete: true,
+  //   },
+  // },
 
   {
     name: "artists",
